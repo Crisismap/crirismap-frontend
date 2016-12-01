6c8f3e3b5f491b978c1650e6fde6313610b2be98
zverev <alexander.zverev@gmail.com>
2016-12-01 15:15:10 +0300

    setup layout with react-toolbox

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react'
-import { Button } from 'react-toolbox'
+import { Layout, Panel, Sidebar, IconButton } from 'react-toolbox'
 
 export default class App extends Component {
+  componentWillMount() {
+    this.setState({
+        sidebarPinned: false
+    })
+  }
+
+  toggleSidebar() {
+    this.setState({ sidebarPinned: !this.state.sidebarPinned });
+  }
+
   render() {
-    return <Button>hello world</Button>
+    return (
+      <Layout>
+        <Panel>
+          <IconButton icon='menu' onClick={() => this.toggleSidebar()} />
+          <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
+              <p>map goes here</p>
+          </div>
+        </Panel>
+        <Sidebar pinned={ this.state.sidebarPinned } width={ 5 }>
+          <div><IconButton icon='close' onClick={() => this.toggleSidebar()}/></div>
+          <div style={{ flex: 1 }}>
+              <p>categories go here</p>
+          </div>
+        </Sidebar>
+      </Layout>
+    )
   }
 }
