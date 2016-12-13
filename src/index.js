5b0db79f264787415ec8c0dafc4c6959ca991284
oleksmarkh <olexiy.markhovsky@gmail.com>
2016-12-13 23:10:17 +0100

    simplify React render invocation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 // @flow
-
 import React from 'react';
 import { render } from 'react-dom';
-import styles from './styles.sass';
+
+import './styles.sass';
 import Layout from './containers/Layout';
 
-window.addEventListener('load', (ev) => {
-  const el = document.createElement('div');
-  el.className = styles.root;
-  document.body.appendChild(el);
-  render(
-    <Layout />
-  , el);
-});
+render(<Layout />, document.getElementById('app'));
